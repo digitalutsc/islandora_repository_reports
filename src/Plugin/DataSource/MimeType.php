5557cde53ee7f7b14ea7c5365649ccdaba27d621
Amy Li <amyamay22@gmail.com>
2023-07-21 11:29:58 -0400

    Change 'Media by MIME type' to 'Files by MIME type'

--- src/Plugin/DataSource/MimeType.php
+++ src/Plugin/DataSource/MimeType.php
@@ -3,7 +3,7 @@
 namespace Drupal\islandora_repository_reports\Plugin\DataSource;
 
 /**
- * Data source that gets media counts by MIME type.
+ * Data source that gets File counts by MIME type.
  */
 class MimeType implements IslandoraRepositoryReportsDataSourceInterface {
 
@@ -18,14 +18,14 @@ class MimeType implements IslandoraRepositoryReportsDataSourceInterface {
    * {@inheritdoc}
    */
   public function getName() {
-    return t('Media by MIME type');
+    return t('Files by MIME type');
   }
 
   /**
    * {@inheritdoc}
    */
   public function getBaseEntity() {
-    return 'media';
+    return 'file';
   }
 
   /**
@@ -39,7 +39,7 @@ class MimeType implements IslandoraRepositoryReportsDataSourceInterface {
    * {@inheritdoc}
    */
   public function getChartTitle($total) {
-    return t('@total media with the selected Media Use terms, grouped by MIME type.', ['@total' => $total]);
+    return t('@total files grouped by MIME type.', ['@total' => $total]);
   }
 
   /**
@@ -47,22 +47,15 @@ class MimeType implements IslandoraRepositoryReportsDataSourceInterface {
    */
   public function getData() {
     $utilities = \Drupal::service('islandora_repository_reports.utilities');
-    $media_use_term_ids = $utilities->getFormElementDefault('islandora_repository_reports_media_use_terms', []);
-
-    if (count($media_use_term_ids) == 0) {
-      return [];
-    }
-
     $entity_type_manager = \Drupal::service('entity_type.manager');
-    $media_storage = $entity_type_manager->getStorage('media');
-    $result = $media_storage->getAggregateQuery()
-      ->groupBy('field_mime_type')
-      ->aggregate('field_mime_type', 'COUNT')
-      ->condition('field_media_use', $media_use_term_ids, 'IN')
+    $file_storage = $entity_type_manager->getStorage('file');
+    $result = $file_storage->getAggregateQuery()
+      ->groupBy('filemime')
+      ->aggregate('filemime', 'COUNT')
       ->execute();
     $format_counts = [];
     foreach ($result as $format) {
-      $format_counts[$format['field_mime_type']] = $format['field_mime_type_count'];
+      $format_counts[$format['filemime']] = $format['filemime_count'];
     }
 
     $this->csvData = [[t('MIME type'), 'Count']];
